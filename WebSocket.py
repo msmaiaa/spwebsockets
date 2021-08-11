cbd6e7cba94e16bc2a55c27e546a067739c542c7
msmaiaa <roxx77344@gmail.com>
2021-08-11 20:09:34 +0000

    add better event support

--- WebSocket.py
+++ WebSocket.py
@@ -1,19 +1,23 @@
 import websocket
 
 class WebSocket:
-    def __init__(self, url, cb=None):
+    def __init__(self, url, on_message=None, on_open=None, on_close=None, on_error=None):
         self.url = url
         self.running = True
         self.ws = None
-        self.cb = cb
+        self.on_message = on_message
+        self.on_open = on_open
+        self.on_close = on_close
+        self.on_error = on_error
 
     def start_socket_connection(self):
         websocket.enableTrace(False)
         self.ws = websocket.WebSocketApp(self.url,
-                                on_open=self.on_open,
-                                on_message=self.on_message if self.cb == None else self.cb,
-                                on_error=self.on_error,
-                                on_close=self.on_close)
+                                on_open=self.on_open if self.on_open == None else self.on_open,
+                                on_message=self.on_message if self.on_message == None else self.on_message,
+                                on_error=self.on_error if self.on_error == None else self.on_error,
+                                on_close=self.on_close if self.on_close == None else self.on_close
+                                )
         self.running = True
         self.ws.run_forever()
 
--- WebSocketThread.py
+++ WebSocketThread.py
@@ -2,14 +2,17 @@ from . import WebSocket
 from listeners.tick import GameThread
 
 class WebSocketThread:
-    def __init__(self, url, cb=None):
-        self.cb = cb
+    def __init__(self, url, on_message=None, on_open=None, on_close=None, on_error=None):
+        self.on_message = on_message
+        self.on_open = on_open
+        self.on_close = on_close
+        self.on_error = on_error
         self.thread = None
         self.url = url
         self.socket = None
 
     def start(self):
-        self.socket = WebSocket(url=''.join(self.url), cb=self.cb)
+        self.socket = WebSocket(url=''.join(self.url), on_message=self.on_message, on_open=self.on_open, on_close=self.on_close, on_error=self.on_error)
         self.thread = GameThread(target=self.socket.start_socket_connection)
         self.thread.start()
     
